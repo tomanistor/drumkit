4d50e2e70eee5e61cfbc89e9fca8ffce123419ad
tomanistor <toma.nistor@gmail.com>
2017-07-09 18:20:19 -0700

    Reorganized project

--- javascript.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('keydown', function(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="$e.keyCode}"]`);
-  if(!audio) return; // stop function when there is no sound
-  audio.currentTime = 0; // rewind audio to the start
-  audio.play();
-  console.log(key)
-});
